6864d992ec343d455f10068db5268f214f8179e9
Akshay T P <akshaythazhepurayil@gmail.com>
2023-01-26 11:52:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showModel, setShowModel] = useState(false)
+  const [modelPermission, setModelPermission] = useState(false)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='btn_div'>
+        <button className='button' onClick={() => setShowModel(true)}>Roles</button>
+      </div>
+    {showModel && (
+      <div className="model">
+        <div className="model_container">
+          <div className="model_title">
+            <p>Add role</p>
+          </div>
+          <div className="model_content">
+            <input className='input' type="text" />
+            <button className="save_button">Save</button>
+          </div>
+          <div className="model_footer">
+            <button className="close_button" onClick={() => setShowModel(false) }>Close</button>
+          </div>
+        </div>
+      </div>
+      )}
+      <div className='btn_div'>
+        <button className='permission_button' onClick={() => setModelPermission(true)}>Permission</button>
+      </div>
+      {modelPermission && (
+      <div className="model">
+        <div className="model_container">
+          <div className="model_title">
+            <p>Add Permissions</p>
+          </div>
+          <div className="model_content">
+            <input className='input' type="text" />
+            <input className='input' type="text" />
+            <button className="save_button">Save</button>
+          </div>
+          <div className="model_footer">
+            <button className="close_button" onClick={() => setModelPermission(false)}>Close</button>
+          </div>
+        </div>
+      </div>
+      )}
     </div>
   );
 }
